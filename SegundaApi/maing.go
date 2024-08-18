6bb50a73db1e20da01815ed058a3a83587a42f02
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2024-08-17 22:47:11 -0300

    Iniciando o Gin

--- SegundaApi/maing.go
+++ SegundaApi/maing.go
@@ -1,1 +1,13 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	api := gin.Default()
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	api.Run() // listen and serve on 0.0.0.0:8080
+}
